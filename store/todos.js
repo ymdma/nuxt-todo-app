19fd62433348fa833567ba09dfd7efb73328c974
ymdma <m.yamada1985@gmail.com>
2020-02-21 22:08:14 +0900

    #

--- store/todos.js
+++ store/todos.js
@@ -15,11 +15,11 @@ export const state = () => ({
 export const actions = {
   // 初期化
   init: firestoreAction(({ bindFirestoreRef }) => { // Action呼出 引数は、本来なら第一引数にはcontextが入るが、こう書くとこれのみ受け取れる
-    bindFirestoreRef('todos', todosRef) //bind(関連付け)したいデータの名前'todos'とコレクションへの参照(2)を渡すとstate.todos(上のステート)に〇〇firestoreのデータが関連付けされる
+    bindFirestoreRef('todos', todosRef) //bind(関連付け)したいデータの名前'todos'とコレクションへの参照(2)を渡すとstate.todos(上のステート)にcloudfirestoreのデータが関連付けされる
   }),
 
   // todoの追加をする時に呼ばれるアクション
-  add: firestoreAction((context,name) => { // 1引数にcontext(不使用)、2引数にtodoの名前を受け取る
+  add: firestoreAction((context, name) => { // 1引数にcontext(不使用)、2引数にtodoの名前を受け取る
     if(name.trim()) { // 入力が空白でないか確認。空白の場合無効に。
       todosRef.add({ // todosRefのaddメソッドを使ってデータをfirestoreに登録。
         name: name,
