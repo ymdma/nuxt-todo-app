ef85c56acb3614a9a3b9d3fb84deb1692ba66aef
ymdma <m.yamada1985@gmail.com>
2020-02-21 19:43:09 +0900

    inisialize_NuxtApp_to_Firebase

---
+++ plugins/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+// firebaseの初期化に必要なパラメータをオブジェクト形式で用意
+const config = {
+  projectUd: process.env.FIREBASE_PROJECT_ID
+  // 適時 APIキーなどを追記
+}
+// 二重の初期化を防ぐ条件分岐を挟む
+if (!firebase.apps.length) {
+  firebase.initializeApp(config) // メソッドを呼び出して初期化
+}
+
+export default firebase //他の場所でも使えるように
