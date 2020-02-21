bdcb7967bbf7f33c934ed792052a11822c24f6f6
ymdma <m.yamada1985@gmail.com>
2020-02-21 19:51:46 +0900

    index.js_[vuexfireMutations]

---
+++ store/index.js
@@ -0,0 +1,7 @@
+import { vuexfireMutations } from 'vuefire'
+// vuexfireの機能を読み込んで使えるようにする
+
+export const mutations = {
+  ...vuexfireMutations // vuexが定義しているミューテーションが使えるようになる
+}
+// * mutationsはルートのストア(index.js)で作る必要がある。(todo.jsに書いたらエラーになる)
